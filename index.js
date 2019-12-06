977496f3a2e036c589c05611ec2e80e8aa2973dd
atvanguard <93arpit@gmail.com>
2019-12-06 18:40:34 +0530

    Polling

--- index.js
+++ index.js
@@ -8,53 +8,45 @@ const redis = require('redis')
 bluebird.promisifyAll(redis);
 const client = redis.createClient(config.get('bee-q.redis'))
 const web3 = new Web3(new HDWalletProvider(process.env.MNEMONIC, process.env.MAIN_RPC));
-const childWeb3 = new Web3(new Web3.providers.WebsocketProvider(process.env.MATIC_RPC));
-let accounts
+const childWeb3 = new Web3(process.env.MATIC_RPC);
+// const childWeb3 = new Web3(new Web3.providers.WebsocketProvider(process.env.MATIC_RPC));
+let accounts, account
 
 const childToRoot = {}
 const withdrawsQName = 'withdraws'
+const withdrawsQ = new Queue(withdrawsQName, config.get('bee-q'));
 
-// function poll() {
-//   config.get('contracts.tokens').forEach(token => {
-//     const childErc20 = new childWeb3.eth.Contract(abi, token.child)
-//     // subscribe to events where we receive tokens
-//     childErc20.events.Transfer({ filter: { to: account }, fromBlock: config.get('fromBlock') }, (err) => {
-//       if (err) console.log(err)
-//     })
-//     .on('connected', function(subscriptionId) {
-//       console.log(`Listening to Transfer(,${account},) events on child contract ${childErc20.options.address}`);
-//     })
-//     .on('data', async event => {
-//       withdrawsQ.createJob(event).save();
-//     })
-//   })
-// }
+async function poll() {
+  const toBlock = await childWeb3.eth.getBlockNumber()
+  const lastProcessed = parseInt(await client.getAsync('lastProcessed'));
+  console.log({ toBlock, lastProcessed })
+  const fromBlock = Math.max(config.get('fromBlock'), lastProcessed)
+  if (fromBlock >= toBlock) return;
+
+  config.get('contracts.tokens').forEach(async token => {
+    const childErc20 = childToRoot[token.child].childErc20;
+    const events = await childErc20.getPastEvents(
+      'Transfer',
+      { filter: { to: account }, fromBlock, toBlock }
+    )
+    // console.log('events', events)
+    events.forEach(event => {
+      withdrawsQ.createJob(event).save();
+    })
+    await client.setAsync('lastProcessed', toBlock);
+  })
+}
 
 async function setup() {
   accounts = await web3.eth.getAccounts()
-  let account = web3.utils.toChecksumAddress(accounts[0])
-  const withdrawsQ = new Queue(withdrawsQName, config.get('bee-q'));
+  account = web3.utils.toChecksumAddress(accounts[0])
   const abi = JSON.parse(JSON.stringify(config.get('contracts.erc20abi')))
 
   config.get('contracts.tokens').forEach(token => {
     const mainErc20 = new web3.eth.Contract(abi, token.root)
     const childErc20 = new childWeb3.eth.Contract(abi, token.child)
-    childToRoot[token.child] = mainErc20
-
-    // subscribe to events where we receive tokens
-    childErc20.events.Transfer({ filter: { to: account }, fromBlock: config.get('fromBlock') }, (err, event) => {
-      if (err) return console.log(err)
-      // console.log(event)
-      withdrawsQ.createJob(event).save();
-    })
-    .on('connected', function(subscriptionId) {
-      console.log(`Listening to Transfer(,${account},) events on child contract ${childErc20.options.address}`);
-    })
-    // .on('data', async event => {
-    //   withdrawsQ.createJob(event).save();
-    // })
+    childToRoot[token.child] = { mainErc20, childErc20 }
   })
-
 
   withdrawsQ.process(async function (job, done) {
     console.log(`Processing job ${job.id}`);
@@ -69,7 +61,7 @@ async function setup() {
       await client.setAsync(key, true)
       if (web3.utils.toBN(amount).gt(web3.utils.toBN(0))) {
         console.log(`Transferring ${web3.utils.fromWei(amount)} to ${recipient}`)
-        await childToRoot[event.address].methods.transfer(recipient, amount).send({
+        await childToRoot[event.address].mainErc20.methods.transfer(recipient, amount).send({
           from: accounts[0],
           gas: 100000,
           nonce: await web3.eth.getTransactionCount(accounts[0], 'pending'),
@@ -94,4 +86,8 @@ async function shouldProcess(key) {
   return !_isProcessed
 }
 
-setup().then(() => console.log('Bridge server initialized'))
+
+setup().then(() => {
+  console.log('Bridge server initialized')
+  setInterval(poll, config.get('pollSeconds') * 1000); // every 5 seconds
+})
